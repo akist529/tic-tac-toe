d61dc18c5032a5edd4bcd5196bef5d6c7e0e975f
Alex Kist <akist0707@gmail.com>
2022-07-30 10:21:48 -0500

    Create barebones functions for gameboard, controller & players

---
+++ script.js
@@ -0,0 +1,18 @@
+var Gameboard = (() => {
+    return {
+        positions: []
+    };
+});
+
+var displayController = (() => {
+    
+});
+
+var Player = (name) => {
+    return {
+        name
+    };
+};
+
+var playerOne = Player('Alex');
+var playerTwo = Player('Johanna');
