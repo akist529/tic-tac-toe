c1b101e1df74215489abf936089c76335f328db6
Alex Kist <akist0707@gmail.com>
2022-07-30 14:27:20 -0500

    Add function to render gameboard array to page

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-var Gameboard = (() => {
+const Gameboard = (() => {
+    const spaces = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+    const render = function() {
+        for (let i = 0; i < spaces.length; i++) {
+            $(".gameBoard").append(`<p>${spaces[i]}</p>`);
+        }
+    }
+
     return {
-        positions: []
+        spaces,
+        render
     };
-});
+})();
 
 var displayController = (() => {
-    
-});
+    const spaces = Gameboard.spaces;
+    var board = [];
 
-var Player = (name) => {
+    for (const space in spaces) {
+        board.push
+    }
+
     return {
-        name
+        board
+    }
+})();
+
+var Player = (role) => {
+    return {
+        role
     };
 };
 
-var playerOne = Player('Alex');
-var playerTwo = Player('Johanna');
+var playerOne = Player('X');
+var playerTwo = Player('O');
+
+console.log(playerOne);
+console.log(playerTwo);
+
+Gameboard.render();
