4fd0d718582ee6f4d6e0f40b621997fe779aa7e9
Alex Kist <akist0707@gmail.com>
2022-08-08 08:30:33 -0500

    Refactored checkWin function

--- script.js
+++ script.js
@@ -134,11 +134,11 @@ const displayController = (() => {
     const checkWin = function(player, arr) {
         // Checks for tie (all cells filled with no winner)
         if (arr.every(cell => cell)) {
-            for (const cell in Array.from($(".gameBoard").children())) {
-                cell.style.backgroundColor = "black";
-                cell.style.color = "white";
+            for (let i = 0; i < arr.length; i++) {
+                Array.from($(".gameBoard").children())[i].style.backgroundColor = "black";
+                Array.from($(".gameBoard").children())[i].style.color = "white";
             }
-
+            
             return true;
         }
 
@@ -146,71 +146,40 @@ const displayController = (() => {
         for (let i = 0; i < arr.length; i+= 3) {
             if (arr[i] === player.role && arr[(i + 1) % arr.length] === player.role && arr[(i + 2) % arr.length] === player.role) {
                 player.score++;
-                Array.from($(".gameBoard").children()[i].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[i].style.color = "white");
-                Array.from($(".gameBoard").children()[i + 1].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[i + 1].style.color = "white");
-                Array.from($(".gameBoard").children()[i + 2].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[i + 2].style.color = "white");
+                showWin(i, i + 1, i + 2);
                 return true;
             }
         }
 
         for (let i = 0; i < arr.length; i++) {
             // Checks vertical rows for a win
-            const vertCheck = [arr[i], arr[(i + 3) % arr.length], arr[(i + 6) % arr.length]];
-            const diagLTRCheck = [arr[0], arr[4], arr[8]];
-            const diagRTLCheck = [arr[2], arr[4], arr[6]];
-            const checkArr = [vertCheck, diagLTRCheck, diagRTLCheck];
-
-            for (const dir of checkArr) {
-                for (let i = 0; i < 3; i++) {
-                    if (dir[i] !== player.role) {
-                        break;
-                    }
-
-                    if (i === 2) {
-                        showWin(dir);
-                    }
-                }
-            }
-
-            /*
             if (arr[i] === player.role && arr[(i + 3) % arr.length] === player.role && arr[(i + 6) % arr.length] === player.role) {
                 player.score++;
-                Array.from($(".gameBoard").children()[i].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[i].style.color = "white");
-                Array.from($(".gameBoard").children()[i + 3].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[i + 3].style.color = "white");
-                Array.from($(".gameBoard").children()[i + 6].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[i + 6].style.color = "white");
+                showWin(i, i + 3, i + 6);
                 return true;
             }
-
             // Checks diagonal left-to-right for a win
-            if (arr[0] === player.role && arr[4] === player.role && arr[8] === player.role) {
+            else if (arr[0] === player.role && arr[4] === player.role && arr[8] === player.role) {
                 player.score++;
-                Array.from($(".gameBoard").children()[0].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[0].style.color = "white");
-                Array.from($(".gameBoard").children()[4].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[4].style.color = "white");
-                Array.from($(".gameBoard").children()[8].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[8].style.color = "white");
+                showWin(0, 4, 8);
                 return true;
             }
 
             // Checks diagonal right-to-left for a win
-            if (arr[2] === player.role && arr[4] === player.role && arr[6] === player.role) {
+            else if (arr[2] === player.role && arr[4] === player.role && arr[6] === player.role) {
                 player.score++;
-                Array.from($(".gameBoard").children()[2].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[2].style.color = "white");
-                Array.from($(".gameBoard").children()[4].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[4].style.color = "white");
-                Array.from($(".gameBoard").children()[6].style.backgroundColor = "black");
-                Array.from($(".gameBoard").children()[6].style.color = "white");
+                showWin(2, 4, 6);
                 return true;
             }
-            */
+        }
+
+        function showWin(index1, index2, index3) {
+            Array.from($(".gameBoard").children()[index1].style.backgroundColor = "black");
+            Array.from($(".gameBoard").children()[index1].style.color = "white");
+            Array.from($(".gameBoard").children()[index2].style.backgroundColor = "black");
+            Array.from($(".gameBoard").children()[index2].style.color = "white");
+            Array.from($(".gameBoard").children()[index3].style.backgroundColor = "black");
+            Array.from($(".gameBoard").children()[index3].style.color = "white");
         }
     }
 
